1473512950a92be8ad1cff2ef6b0f4ec942b22a4
Geet Sethi <geetsethi05@gmail.com>
2025-02-05 22:54:37 +0530

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,61 @@
+const canvas = document.querySelector("#matrix-background");
+const ctx = canvas.getContext("2d");
+
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+const columns = canvas.width / 20;
+const drops = [];
+
+for (let i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+
+function draw() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "rgba(0, 213, 190, 0.3)";
+  ctx.font = "15px monospace";
+
+  for (let i = 0; i < drops.length; i++) {
+    const text = String.fromCharCode(Math.random() * 128);
+    ctx.fillText(text, i * 20, drops[i] * 20);
+
+    if (drops[i] * 20 > canvas.height && Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+
+    drops[i]++;
+  }
+}
+
+setInterval(draw, 33);
+
+const targetDate = new Date("2025-04-06T23:59:59");
+function calculateTimeLeft() {
+  const difference = +targetDate - +new Date();
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  return timeLeft;
+}
+
+function updateCountdown() {
+  const timeLeft = calculateTimeLeft();
+
+  document.querySelector("#countdown-days").textContent = timeLeft.days;
+  document.querySelector("#countdown-hours").textContent = timeLeft.hours;
+  document.querySelector("#countdown-minutes").textContent = timeLeft.minutes;
+  document.querySelector("#countdown-seconds").textContent = timeLeft.seconds;
+}
+
+setInterval(updateCountdown, 1000);
